58868fc158e66edf4b52d4a1973861e9fb09f684
Jorge Guepud <jlguepudb@unal.edu.co>
2023-09-04 21:04:08 -0500

    feat: Se completa la petición HTTP, se agregan interfaces.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http'
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
     providedIn: 'root'
 })
 export class GifsService {
 
+    public gifList: Gif[] = [];
     private _tagsHistory: string[] = [];
     private apiKey: string = 'rG3YC86bxQqm4Zi4jIUem1wQ8aUKAuaG';
     private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
@@ -39,9 +41,11 @@ export class GifsService {
             .set('q', tag)
             .set('limit', '10')
 
-        this.http.get(`${this.serviceUrl}/search`, {params})
-            .subscribe( resp => {
-                console.log(resp);
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
+            .subscribe( (resp) => {
+                this.gifList = resp.data;
+                console.log({gifs: this.gifList});
+                
             });
         
     }
