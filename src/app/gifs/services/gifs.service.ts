7735e42c17c88b68c6a4c52b1ba899d1f5db2a60
Jorge Guepud <jlguepudb@unal.edu.co>
2023-09-05 01:02:25 -0500

    :sparkles: feat: save and load localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,10 +11,14 @@ export class GifsService {
     private _tagsHistory: string[] = [];
     private apiKey: string = 'rG3YC86bxQqm4Zi4jIUem1wQ8aUKAuaG';
     private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
-    private _limit: number = 4;
+    private _limit: number = 8;
+    private localStorageKey = 'history';
 
-    constructor( private http: HttpClient){}
-
+    constructor( private http: HttpClient){
+        this.loadLocalStorage();
+        console.warn(' *** Gifs Service Ready *** ');
+        this.searchTag(this._tagsHistory[0]);
+    }
     
     get tagsHistory(){
         return [...this._tagsHistory];
@@ -24,7 +28,7 @@ export class GifsService {
         this._limit = limit;
     }
 
-    private organizeHistory(tag: string){
+    private organizeHistory(tag: string): void{
         tag = tag.toLowerCase();
 
         if ( this._tagsHistory.includes(tag)){
@@ -33,6 +37,22 @@ export class GifsService {
 
         this._tagsHistory.unshift(tag);
         this._tagsHistory = this.tagsHistory.splice(0,10);
+        this.saveLocalStorage();
+    }
+
+    private saveLocalStorage(): void{
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this._tagsHistory));
+    }
+
+    private loadLocalStorage(): void{
+        if(!localStorage.getItem(this.localStorageKey)){
+            return;
+        }
+
+        this._tagsHistory = JSON.parse(localStorage.getItem(this.localStorageKey)!);
+
+        console.log('localStorage loaded: ', this._tagsHistory);
+        
     }
 
     public searchTag( tag: string): void{
