442bf736c76b6ef870f23712e35b1423b4dbd03d
Jorge Guepud <jlguepudb@unal.edu.co>
2023-09-04 20:17:10 -0500

    feat: servicio con fetch (a lo JS)

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -24,13 +24,17 @@ export class GifsService {
         this._tagsHistory = this.tagsHistory.splice(0,10);
     }
 
-    public searchTag( tag: string): void{
+    public async searchTag( tag: string): Promise<void>{
 
         if(tag.length === 0) return;
 
         this.organizeHistory(tag)
 
-        console.log(this.tagsHistory);
+        fetch('https://api.giphy.com/v1/gifs/search?api_key=rG3YC86bxQqm4Zi4jIUem1wQ8aUKAuaG&q=Fortnite&limit=10')
+            .then(resp => resp.json())
+            .then(data => console.log(data));
+
+
         
     }
 
