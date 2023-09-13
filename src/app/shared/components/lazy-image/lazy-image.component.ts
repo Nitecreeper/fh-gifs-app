36927d0b386dc994b859d658243ba09a70733f2f
Jorge Guepud <jlguepudb@unal.edu.co>
2023-09-12 21:59:46 -0500

    :bug: feat: Se implementa la condición para mostrar el loader

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -8,16 +8,20 @@ import { Component, Input, OnInit } from '@angular/core';
 export class LazyImageComponent implements OnInit{
 
   @Input()
-  public enlace!: string;
+  public url!: string;
   
   @Input()
   public alt: string = '';
+
+  public hasLoaded: boolean = false;
   
   ngOnInit(): void {
-    if(!this.enlace) throw new Error('URL property is required');
-    
-    console.log(this.enlace);
-    
+    if(!this.url) throw new Error('URL property is required');
+  }
+
+  public onLoad():void {
+    console.log('Image loaded');
+    this.hasLoaded = true;    
   }
 
 }
